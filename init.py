112955f7b803a2776120112596d1b90b3e309183
Tarasov Vladyslav <vlad@glaps.fun>
2024-04-05 18:45:53 +0300

    fix init

--- init.py
+++ init.py
@@ -2,17 +2,22 @@ import os
 import subprocess
 import pendulum
 from git import Repo
+from dotenv import load_dotenv
+
 
 GITHUB_REPO_URL = os.getenv("GITHUB_REPO_URL")
 GITHUB_USERNAME = os.getenv("GITHUB_USERNAME")
 GITHUB_EMAIL = os.getenv("GITHUB_EMAIL")
+#load_dotenv()
 
 def clone_and_run():
-    repo_path = "/tmp/test"
+    repo_path = "/tmp/update"
     if os.path.exists(repo_path):
         repo = Repo(repo_path)
+        origin = repo.remote("origin")
+        origin.fetch()
     else:
-        repo = Repo.init(repo_path)
+        repo = Repo.clone_from(GITHUB_REPO_URL,repo_path)
 
     os.chdir(repo_path)
     subprocess.call(["python", "main.py"])
@@ -21,8 +26,10 @@ def clone_and_run():
         git_config.set_value("user", "name", GITHUB_USERNAME)
         git_config.set_value("user", "email", GITHUB_EMAIL)
 
-    repo.index.add_all()
+    repo.index.add(["README.md","data/","img/"])
     repo.index.commit(f"Duty Updates {pendulum.now().to_date_string()}")
+    origin = repo.remote("origin")
+    origin.push()
 
 if __name__ == "__main__":
     clone_and_run()
